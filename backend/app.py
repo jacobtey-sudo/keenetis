4d88ec162b2b03fd0e1a3d969a06031778e460f4
jacobtey-sudo <jacobtey@gmail.com>
2026-04-18 01:02:14 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,24 @@
+from fastapi import FastAPI
+from router import ssh
 
+app = FastAPI()
+
+# 🌐 ISP
+@app.post("/isp")
+def isp():
+    return {"result": ssh("ip route replace default dev GigabitEthernet0/Vlan4")}
+
+# 🔐 VPN1
+@app.post("/vpn1")
+def vpn1():
+    return {"result": ssh("ip route replace default dev Wireguard1")}
+
+# 🔐 VPN2
+@app.post("/vpn2")
+def vpn2():
+    return {"result": ssh("ip route replace default dev Wireguard2")}
+
+# 📊 status
+@app.get("/status")
+def status():
+    return {"result": ssh("ip route")}
