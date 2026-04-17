bf0e553e902102673b2e7f7a5c002f89acdb0f8e
jacobtey-sudo <jacobtey@gmail.com>
2026-04-18 01:01:43 +0300

    Add SSH function to execute commands remotely

--- backend/router.py
+++ backend/router.py
@@ -1,1 +1,6 @@
+import subprocess
+from config import *
 
+def ssh(cmd: str):
+    full = f'ssh -p {ROUTER_SSH_PORT} {ROUTER_USER}@{ROUTER_HOST} "{cmd}"'
+    return subprocess.check_output(full, shell=True).decode()
